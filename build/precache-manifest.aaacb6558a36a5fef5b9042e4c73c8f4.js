ab055511933b5925eb7133be4f2e979ffbe689a0
danielbevziuc <bevziuc.daniel@gmail.com>
2020-09-27 20:44:29 +0100

    build a covid 19 tracker

---
+++ build/precache-manifest.aaacb6558a36a5fef5b9042e4c73c8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26340120db714b003f490cb94268241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "38d5481bc3e78d2663bf",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d5481bc3e78d2663bf",
+    "url": "/static/js/main.b1f39876.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
